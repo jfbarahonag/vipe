2e5dd5734703f0196932df4c11c148906dabe671
jfbarahonag <jfbarahonag@unal.edu.co>
2023-02-16 00:26:16 -0500

    topbar component was added to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react'
+import { Link } from 'react-router-dom';
 import Wrapper from '../common/Wrapper';
 
 /**
@@ -7,17 +7,96 @@ import Wrapper from '../common/Wrapper';
  * -------------------------------------------------------------------------------------------------
  */
 
+const topBarLinks = {
+  left: [
+    {
+      title: "FAQs",
+      path: "/faq",
+    },
+    {
+      title: "Help",
+      path: "/help",
+    },
+    {
+      title: "Support",
+      path: "/support",
+    },
+  ],
+  right: [
+    {
+      title: "facebook",
+      path: "/",
+      fa:'fab fa-facebook-f'
+    },
+    {
+      title: "twiter",
+      path: "/",
+      fa:'fab fa-twitter'
+    },
+    {
+      title: "instagram",
+      path: "https://www.instagram.com",
+      fa:'fab fa-instagram'
+    },
+
+  ],
+}
+
 const Header = () => {
   return (
-    <header>
-      <Wrapper>
-        <h1>JAJAJA</h1>
-        <h2>kakakaak</h2>
+    <Wrapper>
+      <header>
         {/* // topbar */}
+        <div className='container-fluid bg-primary py-3 d-none d-md-block'>
+          <div className="container">
+            <div className="row">
+              {/* left navbar */}
+              <nav className="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
+                <ul className="d-inline-flex align-items-center" style={{ listStyle: 'none' }}>
+                  {
+                    topBarLinks.left.map(item =>
+                      <li
+                        key={item.title}
+                      >
+                        <Link
+                          target={'_self'}
+                          className='text-white pr-3'
+                          to={item.path}
+                        >
+                          {item.title}
+                        </Link>
+                      </li>
+                    )
+                  }
+                </ul>
+              </nav>
+              {/* right navbar */}
+              <nav className="col-md-6 text-center text-lg-right">
+                <ul className="d-inline-flex align-items-center" style={{ listStyle: 'none' }}>
+                  {
+                    topBarLinks.right.map(item =>
+                      <li
+                        key={item.title}
+                      >
+                        <Link
+                          target={'_blank'}
+                          className='text-white pr-3'
+                          to={item.path}
+                        >
+                          <i className={item.fa}></i>
+                        </Link>
+                      </li>
+                    )
+                  }
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
         {/* // navbar */}
         {/* // hero slider */}
-      </Wrapper>
-    </header>
+      </header>
+    </Wrapper>
   )
 }
 
