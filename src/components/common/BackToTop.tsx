25104410007d90815effd04ec1c833d755cd1177
jfbarahonag <jfbarahonag@unal.edu.co>
2023-03-01 23:09:27 -0500

    scroll to top component was created and integrated to home view

---
+++ src/components/common/BackToTop.tsx
@@ -0,0 +1,10 @@
+
+const BackToTop = () => {
+  return (
+    <a href="#" className="btn btn-secondary px-2 back-to-top">
+      <i className="fa fa-angle-double-up"></i>
+    </a>
+  )
+}
+
+export default BackToTop;
